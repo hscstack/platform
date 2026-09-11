4ff53d224f64eba2dacfe41f3ea41459174f2452
Tajim <com.tajim@gmail.com>
2026-09-11 17:51:21 +0600

    feat(chat): auto-link platform URLs in chat messages

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,6 +38,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
+            'app_url' => config('app.url'),
             'app_version' => config('app.version'),
             'auth' => [
                 'user' => $request->user(),
